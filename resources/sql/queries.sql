431eed11b33f43af5621a2a17004876d6ff149b2
Arul Prakash Pugazhendi <spv.arul@gmail.com>
2017-09-13 08:37:28 +0530

    Completed SQL queries for the remaining tables

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -67,27 +67,71 @@ WHERE posted_by = :id
 -- :doc delete a user given the id
 DELETE FROM posts
 WHERE id = :id
-
 --------------------------COMMENTS-------------------------
 
--- :name create-comments! :! :n
+-- :name create-comments! :! :1
 -- :doc creates a new post
 INSERT INTO comments
-(id, title, url, description, created, posted_by, posted_in)
-VALUES (:id, :title, :url, :description, :created, :posted_by, :posted_in)
+(id, description, votes, created, posted_to, replied_to)
+VALUES (:id, :description, :votes, :created, :posted_to, :replied_to)
 
--- :name update-posts! :! :n
+-- :name update-comments! :! :1
 -- :doc update an existing group's description or rules
-UPDATE posts
-SET description = :description, rules = :rules, changed = :changed
+UPDATE comments
+SET description = :description
 WHERE id = :id
 
 -- :name get-user-comments :? :n
 -- :doc retrieve all the posts posted by the user
-SELECT * FROM posts
+SELECT * FROM comments
 WHERE posted_by = :id
 
--- :name delete-group! :! :n
+-- :name get-post-comments :? :n
+-- :doc retrieve all the posts posted by the user
+SELECT * FROM comments
+WHERE posted_in = :id
+
+-- :name delete-comment! :! :n
 -- :doc delete a user given the id
-DELETE FROM posts
+UPDATE comments
+SET description = "[deleted]"
 WHERE id = :id
+
+
+--------------------------SUBSRIPTIONS-------------------------
+
+-- :name create-sub! :! :1
+-- :doc creates a new subscription
+INSERT INTO subscriptions
+(id, subscribed_to, subscriber, created)
+VALUES (:id, :subscribed_to, :subscriber, :created)
+
+-- :name get-user-subs :? :n
+-- :doc retrieve all the subscriptions by an user
+SELECT subscribed_to FROM subscriptions
+WHERE subscriber = :id
+
+-- :name delete-sub! :! :1
+-- :doc delete a sub given the id
+DELETE subscriptions
+WHERE subscribed_to =:group_id AND subscriber = :user_id
+
+--------------------------PRIVILEGS-------------------------
+
+-- :name create-privilege! :! :1
+-- :doc creates a new subscription
+INSERT INTO privileges
+(id, subscription, privilege, created)
+VALUES (:id, :subscription, :privilege, :created)
+
+-- :name get-sub-privileges :? :n
+-- :doc list the privileges of a subscription by an user
+SELECT privilege FROM privileges
+WHERE subscription = :subscription_id
+
+-- :name delete-privilege! :! :1
+-- :doc revoke a privilege for a user on that subscription
+DELETE privileges
+WHERE subscription =
+(SELECT id FROM subscriptions
+WHERE subscribed_to =:group_id AND subscriber = :user_id)
