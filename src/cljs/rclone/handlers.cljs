16766cb58209e472784edba9d6fa386f1944660a
Arul Prakash Pugazhendi <A.PUGAZHENDI@amer.zurich.corp>
2017-09-18 18:13:26 +0530

    Some changes in graphql client handler

--- src/cljs/rclone/handlers.cljs
+++ src/cljs/rclone/handlers.cljs
@@ -1,6 +1,8 @@
 (ns rclone.handlers
   (:require [rclone.db :as db]
-            [re-frame.core :refer [dispatch reg-event-db]]))
+            [ajax.core :refer [GET POST json-response-format json-request-format]]
+            [re-frame.core :refer [dispatch reg-event-db reg-event-fx]]
+            [day8.re-frame.http-fx]))
 
 (reg-event-db
   :initialize-db
@@ -13,6 +15,30 @@
     (assoc db :page page)))
 
 (reg-event-db
-  :set-docs
-  (fn [db [_ docs]]
-    (assoc db :docs docs)))
+ :set-docs
+ (fn [db [_ docs]]
+   (assoc db :docs docs)))
+
+(reg-event-db
+ :good-http-result
+ (fn [db [_ result]]
+   (js/console.log result)))
+
+(reg-event-db
+ :bad-http-result
+ (fn [db [_ result]]
+   (js/console.log result)))
+
+(reg-event-fx
+ :graphql-handler
+ (fn [{:keys [db]} _]
+   {    :http-xhrio {:method          :get
+                     :uri             "/graphql"
+                     :params           {"query"  "{user(id:\"arul\") {first_name}}"}
+                     :response-format (json-response-format {:keywords? true})
+                     :on-success      [:good-http-result]
+                     :on-failure      [:bad-http-result]}}))
+
+;;:format  {:content-type "application/graphql"}
+
+(GET "/graphql" {:params {"query"  "{user(id:\"arul\") {first_name}}"}})
