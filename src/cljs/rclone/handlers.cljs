88e0e81543bb79bdf275fe0575c8d324fecca0b3
Arul Prakash Pugazhendi <spv.arul@gmail.com>
2017-09-25 09:33:40 +0530

    re-frame graphql handlers are working

--- src/cljs/rclone/handlers.cljs
+++ src/cljs/rclone/handlers.cljs
@@ -1,8 +1,25 @@
 (ns rclone.handlers
   (:require [rclone.db :as db]
-            [re-frame.core :refer [dispatch reg-event-db reg-event-fx]]
+            [re-frame.core :refer [dispatch reg-event-db reg-event-fx ->interceptor get-coeffect assoc-effect assoc-coeffect debug]]
             [day8.re-frame.http-fx]            
-            [ajax.core :refer [GET POST json-response-format json-request-format url-request-format]]))
+            [ajax.core :refer [GET POST json-response-format json-request-format url-request-format]]
+            [venia.core :as v]))
+
+(def generate-query
+  (->interceptor
+   :id :generate-query
+   :before (fn [context]
+             (let [params (get-in context [:coeffects :event])
+                   query (v/graphql-query {:venia/queries [(second params)]})
+                   t2 (js/console.log query)]
+               (assoc-in context [:coeffects :event] [(first params) query])))))
+
+(def test-int
+  (->interceptor
+   :id :test-int
+   :before (fn [context]
+             (let [t2 (js/console.log "Interceptor hit")]
+               context))))
 
 (reg-event-db
  :initialize-db
@@ -10,9 +27,9 @@
    db/default-db))
 
 (reg-event-db
-  :set-active-page
-  (fn [db [_ page]]
-    (assoc db :page page)))
+ :set-active-page 
+ (fn [db [_ page]]
+   (assoc db :page page)))
 
 (reg-event-db
  :set-docs
@@ -20,21 +37,35 @@
    (assoc db :docs docs)))
 
 (reg-event-db
-  :good-http-result
-  (fn [db [_ result]]
-    (js/console.log result)))
+ :good-http-result
+ (fn [db [_ result]]
+   (js/console.log (str "Success  :  " result))))
 
 (reg-event-db
  :bad-http-result
  (fn [db [_ result]]
-   (js/console.log result)))
+   (js/console.log (str "Failure  :  " result))))
 
 (reg-event-fx
- :graphql-handler
- (fn [{:keys [db]} _]
+ :query-server
+ [generate-query] 
+ (fn [{:keys [db]} [_ query]]
    {:http-xhrio {:method          :get
                  :uri             "/graphql"
-                 :params          {"query" "{user(id:\"arul\") {first_name}}"}
+                 :temp (js/console.log (str "In Query Server " query))
+                 :params          {"query" query}
+                 :format          (url-request-format)
+                 :response-format (json-response-format {:keywords? true})
+                 :on-success      [:good-http-result]
+                 :on-failure      [:bad-http-result]}}))
+
+(reg-event-fx
+ :mutate-server
+ [debug generate-query]
+ (fn [{:keys [db]} [_ mutation]]
+   {:http-xhrio {:method          :get
+                 :uri             "/graphql"
+                 :params          {"mutation" mutation}
                  :format          (url-request-format)
                  :response-format (json-response-format {:keywords? true})
                  :on-success      [:good-http-result]
